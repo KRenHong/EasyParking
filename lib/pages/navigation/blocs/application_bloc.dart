d67fe6023296967762b336998958c001fe6691cb
KAE REN <hkr0528@gmail.com>
2023-03-10 06:12:16 +0800

    ✅ Add Marrkerss

--- lib/pages/navigation/blocs/application_bloc.dart
+++ lib/pages/navigation/blocs/application_bloc.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 
+import 'package:easyparking/pages/navigation/models/geometry.dart';
+import 'package:easyparking/pages/navigation/models/location.dart';
+import 'package:easyparking/pages/navigation/models/place.dart';
 import 'package:easyparking/pages/navigation/models/place_search.dart';
 import 'package:easyparking/pages/navigation/services/geolocator_service.dart';
+import 'package:easyparking/pages/navigation/services/marker_service.dart';
 import 'package:easyparking/pages/navigation/services/places_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
@@ -10,16 +14,27 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class ApplicationBloc with ChangeNotifier {
   final geoLocatorService = GeolocatorService();
   final placesService = PlacesService();
+  final markerService = MarkerService();
+
 
   
   //Variable
-  Position currentLocation = Position(longitude: 0, latitude:  0, timestamp: DateTime.now(), accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0);
+  Position? currentLocation;
   List<PlaceSearch> searchResults = [];
+  StreamController<Place> selectedLocation = StreamController<Place>();
+  StreamController<LatLngBounds> bounds = StreamController<LatLngBounds>();
+  Place? selectedLocationStatic;
+  String placeType = "";
+  List<Place> placeResults = [];
+  List<Marker> markers = <Marker>[];
+
 
 
   ApplicationBloc() {
     _init();
   }
+  
+  // get markerService => null;
 
   Future<void> _init() async {
     await setCurrentLocation();
@@ -27,6 +42,16 @@ class ApplicationBloc with ChangeNotifier {
 
   Future<void> setCurrentLocation() async {
     currentLocation = await geoLocatorService.getCurrentLocation();
+    selectedLocationStatic = Place(
+        name: "",
+        geometry: Geometry(
+          location: Location(
+            lat: currentLocation?.latitude ?? 0.0,
+            lng: currentLocation?.longitude ?? 0.0,
+          ),
+        ),
+        vicinity: "",
+      );
     notifyListeners();
   }
 
@@ -35,4 +60,54 @@ class ApplicationBloc with ChangeNotifier {
     searchResults = await placesService.getAutocomplete(searchTerm);
     notifyListeners();
   }
+
+  
+  setSelectedLocation(String placeId) async {
+    var sLocation = await placesService.getPlace(placeId);
+    selectedLocation.add(sLocation);
+    selectedLocationStatic = sLocation;
+    searchResults = [];
+    notifyListeners();
+  }
+
+  togglePlaceType(String value, bool selected) async {
+    if (selected) {
+      placeType = value;
+      print("placeType: " + placeType);
+    } else {
+      placeType = "";
+    }
+
+    if (placeType != "") {
+      var places = await placesService.getPlaces(
+          selectedLocationStatic!.geometry.location.lat,
+          selectedLocationStatic!.geometry.location.lng, placeType);
+      print("Length: " + places.toString());
+      markers= [];
+      if (places.length > 0) {
+        var newMarker = markerService.createMarkerFromPlace(places[0],false);
+        markers.add(newMarker);
+      }
+
+      var locationMarker = markerService.createMarkerFromPlace(selectedLocationStatic!,true);
+      markers.add(locationMarker);
+
+      var _bounds = markerService.bounds(Set<Marker>.of(markers));
+      bounds.add(_bounds!);
+
+      notifyListeners();
+    } else {
+      print("Length: 0");
+    }
+  }
+
+
+
+@override
+void dispose() {
+  selectedLocation.close();
+  bounds.close();
+  super.dispose();
+  }
+
 }
--- lib/pages/navigation/navigation_page.dart
+++ lib/pages/navigation/navigation_page.dart
@@ -1,8 +1,10 @@
 import 'package:easyparking/pages/navigation/blocs/application_bloc.dart';
+import 'package:easyparking/pages/navigation/models/place.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+
 
 
 class NavigationPage extends StatefulWidget {
@@ -13,12 +15,46 @@ class NavigationPage extends StatefulWidget {
 }
 
 class _NavigationPageState extends State<NavigationPage> {
+  Completer<GoogleMapController> _mapController = Completer();
+  StreamSubscription? locationSubscription;
+  StreamSubscription? boundsSubscription;
+  final _locationController = TextEditingController();
+
+
+
 
   @override
   void initState() {
     // TODO: implement initState
     final applicationBloc = Provider.of<ApplicationBloc>(context, listen: false);
+
+    //Listen for selected Location
+    locationSubscription = applicationBloc.selectedLocation.stream.listen((place) {
+      if (place != null) {
+        _locationController.text = place.name;
+        _goToPlace(place);
+      } else
+        _locationController.text = "";
+    });
+
+    applicationBloc.bounds.stream.listen((bounds) async {
+      final GoogleMapController controller = await _mapController.future;
+      controller.animateCamera(CameraUpdate.newLatLngBounds(bounds, 50));
+    });
+
+
     super.initState();
+  }
+
+    @override
+  void dispose() {
+    final applicationBloc =
+        Provider.of<ApplicationBloc>(context, listen: false);
+    applicationBloc.dispose();
+    _locationController.dispose();
+    locationSubscription?.cancel();
+    boundsSubscription?.cancel();
+    super.dispose();
   }
 
 
@@ -28,36 +64,215 @@ class _NavigationPageState extends State<NavigationPage> {
 
     return Scaffold(
       body:(applicationBloc.currentLocation == null) 
-          ? Center(
+          ? const Center(
               child: CircularProgressIndicator(),
               )
-      : Stack(
-        children: [
-          
-        GoogleMap(
-        mapType: MapType.normal,
-        myLocationEnabled: true,
-        initialCameraPosition: 
-          CameraPosition(target: LatLng(applicationBloc.currentLocation.latitude, applicationBloc.currentLocation.longitude),
-          zoom: 14),
-      ),
+      : ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      textCapitalization: TextCapitalization.words,
+                      decoration: const InputDecoration(
+                        hintText: 'Search by City',
+                        suffixIcon: Icon(Icons.search),
+                      ),
+                      onChanged: (value) => applicationBloc.searchPlaces(value),
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        height: 500.0,
+                        child: GoogleMap(
+                          mapType: MapType.normal,
+                          myLocationEnabled: true,
+                          myLocationButtonEnabled: true,
+                          initialCameraPosition: CameraPosition(
+                            target: LatLng(
+                                applicationBloc.currentLocation?.latitude ?? 0.0,
+                                applicationBloc.currentLocation?.longitude ?? 0.0),
+                            zoom: 14,
+                          ),
+                          onMapCreated: (GoogleMapController controller) {
+                            _mapController.complete(controller);
+                          },
+                          // markers: Set<Marker>.of(applicationBloc.markers),
+                            markers: Set<Marker>.of([
+                                    Marker(
+                                      markerId: MarkerId('parking1'),
+                                      position: LatLng(3.0738, 101.5183),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking2'),
+                                      position: LatLng(3.0400342456486484, 101.57986118588914),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking3'),
+                                      position: LatLng(3.041470810386873, 101.58054291955547),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking4'),
+                                      position: LatLng(3.0487989720873734, 101.5777534221093),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking5'),
+                                      position: LatLng(3.0546057548451553, 101.59468352583382),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
 
 
-      Container(
-        margin: const EdgeInsets.only(top: 70 ,left: 30.0, right: 30.0),
-        child: const TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            filled: true,
-            fillColor: Colors.white,
-            labelText: 'Search here',
-          ),
-        )
-      )
-        ],
-      )
+                                    Marker(
+                                      markerId: MarkerId('parking6'),
+                                      position: LatLng(3.053962939111916, 101.57871901737263),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+
+                                    Marker(
+                                      markerId: MarkerId('parking7'),
+                                      position: LatLng(3.046270547674886, 101.57968461264247),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+
+                                    Marker(
+                                      markerId: MarkerId('parking8'),
+                                      position: LatLng(3.0444063665408168, 101.59706532749938),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+
+                                    Marker(
+                                      markerId: MarkerId('parking9'),
+                                      position: LatLng(3.0412779633666167, 101.59258067391285),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+
+                                    Marker(
+                                      markerId: MarkerId('parking10'),
+                                      position: LatLng(3.0663729271715154, 101.58763874767999),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+
+                                    Marker(
+                                      markerId: MarkerId('parking11'),
+                                      position: LatLng(3.0400342456486484, 101.57986118588914),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking12'),
+                                      position: LatLng(3.0686192357061466, 101.57849311023784),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking13'),
+                                      position: LatLng(3.064458037482437, 101.60460243003234),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking14'),
+                                      position: LatLng(3.070718241053567, 101.60297981693778),
+                                      infoWindow: InfoWindow(title: 'Arena MBSJ'),
+                                    ),
+
+                                    Marker(
+                                      markerId: MarkerId('parking15'),
+                                      position: LatLng(3.0731, 101.6071),
+                                      infoWindow: InfoWindow(title: 'Parking 2'),
+                                    ),
+                                  ]),
+
+
+                        ),
+                      ),
+                      if (applicationBloc.searchResults != null &&
+                          applicationBloc.searchResults.length != 0)
+                        Container(
+                            height: 200.0,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(.6),
+                                backgroundBlendMode: BlendMode.darken)
+                                ),
+                      if (applicationBloc.searchResults != null)
+                        Container(
+                          height: 200.0,
+                          child: ListView.builder(
+                              itemCount: applicationBloc.searchResults.length,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  title: Text(
+                                    applicationBloc
+                                        .searchResults[index].description,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  onTap: () {
+                                    applicationBloc.setSelectedLocation(
+                                        applicationBloc
+                                            .searchResults[index].placeId);
+                                  },
+                                );
+                              }),
+                        ),
+                    ],   
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Find Nearest',
+                        style: TextStyle(
+                            fontSize: 25.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Wrap(
+                      spacing: 8.0,
+                      children: [
+                        FilterChip(
+                          label: Text('Campground'),
+                          onSelected: (val) => applicationBloc.togglePlaceType('campground', val),
+                          selected: applicationBloc.placeType  == 'campground',
+                          selectedColor: Colors.blue,
+                        ),
+
+                         FilterChip(
+                          label: Text('Parking'),
+                          onSelected: (val) {
+                            print(val.toString());
+                            return applicationBloc.togglePlaceType('parking', val);
+                          },
+                          selected: applicationBloc.placeType  == 'parking',
+                          selectedColor: Colors.blue,
+                        ),
+                      ]
+                  ),
+                  
+                ]
+              ),
+    
     );
   }
 
+  Future<void> _goToPlace(Place place) async {
+    final GoogleMapController controller = await _mapController.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: LatLng(
+                place.geometry.location.lat, place.geometry.location.lng),
+            zoom: 14.0),
+      ),
+    );
+  }
 }
